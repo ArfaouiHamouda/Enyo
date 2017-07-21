1795f2aee6c07c3ff54dbb39cd8788e137720fbe
Arfaoui Hamouda <geekzonetn@gmail.com>
2017-07-21 14:32:18 +0100

    mixit up plugin

---
+++ assets/js/plugins.js
@@ -0,0 +1,14 @@
+/*
+Author : Arfaoui Hamouda
+Version : 0.1
+ */
+$(document).ready(function(){
+
+    // Trigger MixitUp
+    var mixer = mixitup('#plugin');
+    // Adjust Shuffle Links
+    $(".menu li").click(function () {
+        $(this).addClass("selected").siblings().removeClass("selected");
+    });
+
+})
