f590ab77ec30cd0881a8ca316ed954aac4e0134e
Arfaoui Hamouda <geekzonetn@gmail.com>
2017-07-21 21:01:59 +0100

    Responsive Design , Smooth and Nice Scroll Plugins added

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -20,4 +20,29 @@ $(document).ready(function(){
     // Trigger WOW.JS
     new WOW().init();
 
+    // Nice Scroll
+    $("html").niceScroll({
+        cursorcolor: "#25365d", // change cursor color in hex
+        cursorwidth: "7px", // cursor width in pixel (you can also write "5px")
+        cursorborder: "none", // css definition for cursor border
+        cursorborderradius: "0px", // border radius in pixel for cursor
+    });
+
+    //SmoothSCroll
+    var scroll = new SmoothScroll('a[href*="#"]',{
+        // Speed & Easing
+        speed: 1300, // Integer. How fast to complete the scroll in milliseconds
+        offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+        easing: 'easeOutQuart', // Easing pattern to use
+    });
+
+    // Caching The Scroll Top Element
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 700) {
+            $("#scroll").show();
+        } else {
+            $("#scroll").hide();
+        }
+    });
+
 })
